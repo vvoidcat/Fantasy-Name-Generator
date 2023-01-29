9187b95fbbd100d13690827f17f7c878338e9fee
vvoidcat <ekaterina.akchurina98@gmail.com>
2023-01-29 17:50:11 +0300

    MainWindow xaml split

---
+++ src/ui/MainPanel/GeneratedMainNameItem.xaml.cs
@@ -0,0 +1,33 @@
+﻿using NAMEGEN.Ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NAMEGEN.Ui {
+    /// <summary>
+    /// Interaction logic for GeneratedMainNameItem.xaml
+    /// </summary>
+    public partial class GeneratedMainNameItem : UserControl {
+        public static readonly DependencyProperty titleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(GeneratedMainNameItem), new PropertyMetadata(string.Empty));
+        public string Title {
+            get { return (string)GetValue(titleProperty); }
+            set { SetValue(titleProperty, value); }
+        }
+
+        public GeneratedMainNameItem() {
+            InitializeComponent();
+        }
+    }
+}
